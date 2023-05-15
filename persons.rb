7eda2d08ff244eaee6553559288da87b4194a7c3
johnie261 <njorogejohn261@gmail.com>
2023-05-15 23:07:11 +0300

    Fix linters

--- persons.rb
+++ persons.rb
@@ -1,33 +1,32 @@
 class Person
-    #getters for id, name and age
-    attr_reader :id, :name, :age, :parent_permission
-    
-    def initialize(name: "Unknown", age:, parent_permission: true)
-        @id = Random.rand(1..1000)
-        @name = name
-        @age = age
-        @parent_permission = parent_permission
-    end
+  # getters for id, name and age
+  attr_reader :id, :name, :age, :parent_permission
 
-    #setter for name and age
-    def name=(new_name)
-        @name = new_name
-    end
+  def initialize(name: "Unknown", age:, parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
 
-    def age=(new_age)
-        @age = new_age
-    end
+  # setter for name and age
+  def name=(new_name)
+    @name = new_name
+  end
 
-    #public method which is calling the private method
-    def can_use_services?
-        of_age? || @parent_permission
-    end
+  def age=(new_age)
+    @age = new_age
+  end
 
-    #declare a private method
-    private
+  # public method which is calling the private method
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
-    def of_age?
-        @age >= 18 ? true : false
-    end
+  # declare a private method
+  private
+
+  def of_age?
+    @age >= 18 ? true : false
+  end
 end
-
--- student.rb
+++ student.rb
@@ -1,15 +1,14 @@
 require_relative 'persons'
 
 class Student < Person
-    attr_reader :classroom
-    
-    def initialize(name: "Unknown", age:, parent_permission: true, classroom:)
-        super(name: name, age: age, parent_permission: parent_permission)
-        @classroom = classroom
-    end
+  attr_reader :classroom
 
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  def initialize(name: "Unknown", age:, parent_permission: true, classroom:)
+    super(name: name, age: age, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
-
