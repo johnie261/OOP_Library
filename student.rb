6df3a38ed9287cb7b474fd349a4f06e728d1cd51
johnie261 <njorogejohn261@gmail.com>
2023-05-16 19:32:54 +0300

    Create a class book

--- student.rb
+++ student.rb
@@ -8,7 +8,7 @@ class Student < Person
     @classroom = classroom
   end
 
-  def classroom=(classroom)
+  def add_classroom(classroom)
     @classroom = classroom
     classroom.students << self unless classroom.students.include?(self)
   end
