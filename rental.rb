b9fa8b35646bfba0bdaf2ffb7fc2a7deebea6427
johnie261 <njorogejohn261@gmail.com>
2023-05-16 21:27:10 +0300

    Make changes

--- rental.rb
+++ rental.rb
@@ -4,13 +4,12 @@ class Rental
   def initialize(date)
     @date = date
 
-    @book = book
-    @person = person
-
     # Establishes the belongs-to association: a rental belongs to a book
+    @book = book
     book.add_rental(self)
 
     # Establishes the belongs-to association: a rental belongs to a person
+    @person = person
     person.add_rental(self)
   end
 end
